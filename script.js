6ab0045d51dcc16722a506e2caeffadb8d2a1362
Your Name <you@example.com>
2022-06-20 15:21:58 +0600

    completed designing the website

---
+++ script.js
@@ -0,0 +1,15 @@
+let save = document.getElementById("save-record")
+
+let reset = document.getElementById("reset")
+
+let deleteData = document.getElementById("delete")
+
+let deleteAll = document.getElementById("delete-all")
+
+save.addEventListener("click", "hello world")
+
+save.addEventListener("click", "hello world")
+
+save.addEventListener("click", "hello world")
+
+save.addEventListener("click", "hello world")
